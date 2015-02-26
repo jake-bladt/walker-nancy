6b0d663b9bbbec12e1911778c99bee3e699b3f54
Jake <jake@jakebladt.com>
2015-02-26 17:00:24 -0500

    create user class

---
+++ walker-nancy/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Walker.Nancy.Entities
+{
+    public class User
+    {
+        public virtual int Id { get; protected set; }
+        public virtual string Nickname { get; set; }
+        public virtual string Password { get; set; } // Obviously horrible to keep in plaintext. Fix before letting anyone else use this system.
+    }
+}
