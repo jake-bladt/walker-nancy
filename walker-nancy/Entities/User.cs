88785761b990a8ba677e8acf5638ba7c3ce755cb
Jake <jake@jakebladt.com>
2015-02-26 17:14:24 -0500

    map user entity

--- walker-nancy/Entities/User.cs
+++ walker-nancy/Entities/User.cs
@@ -3,12 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
+using FluentNHibernate;
+using FluentNHibernate.Mapping;
+using NHibernate;
+
 namespace Walker.Nancy.Entities
 {
     public class User
     {
         public virtual int Id { get; protected set; }
         public virtual string Nickname { get; set; }
-        public virtual string Password { get; set; } // Obviously horrible to keep in plaintext. Fix before letting anyone else use this system.
+        public virtual string Password { get; set; } // Obviously horrible to keep in plaintext. Fix before golive.
     }
+
+    public class UserMap: ClassMap<User>
+    {
+        public UserMap()
+        {
+            Id(x => x.Id);
+            Map(x => x.Nickname);
+            Map(x => x.Password);
+        }
+    }
+    
 }
