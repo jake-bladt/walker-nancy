3560d424f3052b00bda08c353fb816d2f9dba806
Jake <jake@jakebladt.com>
2015-02-26 15:33:27 -0500

    +home view

---
+++ walker-nancy/Modules/HomeModule.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using Nancy;
+
+namespace Walker.Nancy.Modules
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get["/"] = prms => View["Index"];
+        }
+    }
+}
